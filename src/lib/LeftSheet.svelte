1e524b8746af25d84c18619c1ad2906fa16d6dca
Samarth <samarth.tumdi@gmail.com>
2025-05-16 11:50:21 +0530

    LeftSheet contents disabled when the sheet is offscreen, better tab focus

--- src/lib/LeftSheet.svelte
+++ src/lib/LeftSheet.svelte
@@ -13,18 +13,21 @@
   }
 </script>
 
+
 <aside id="left-sheet-wrapper" class={["fixed top-0 w-[40%] h-full transition-[left] duration-[500ms]", sheetMode ? 'left-0' : '-left-[40%]']}>
   <div id="left-sheet" class="w-full h-full bg-kwdr-fg border-r-4 border-r-kwdr-fg--muted overflow-y-scroll custom-scrollbar">
-    <h2 class="p-2 sticky top-0 bg-kwdr-fg--muted text-2xl text-kwdr-bg">Subjects</h2>
+    {#if sheetMode}
+      <h2 class="p-2 sticky top-0 bg-kwdr-fg--muted text-2xl text-kwdr-bg">Subjects</h2>
       {#each CodebaseEntries as [subjectName, codeItem]}
-        <article>
-          <h2 class="subject px-2">{subjectName}</h2>
-          {#each Object.entries(codeItem) as [projectName, projectBody]}
-            <button class="mx-2 block hover:bg-kwdr-fg--muted/50 transition-[background] duration-500 cursor-pointer" onclick={() => {handleProjectClick(subjectName, projectName, projectBody)}}>&nbsp;{projectName}&nbsp;</button>
-          {/each}
-        </article>
+      <article>
+        <h2 class="subject px-2">{subjectName}</h2>
+        {#each Object.entries(codeItem) as [projectName, projectBody]}
+        <button class="mx-2 block hover:bg-kwdr-fg--muted/50 transition-[background] duration-500 cursor-pointer" onclick={() => {handleProjectClick(subjectName, projectName, projectBody)}}>&nbsp;{projectName}&nbsp;</button>
+        {/each}
+      </article>
       {/each}
-  </div>
+    {/if}
+    </div>
 
   <button id="left-sheet-ctrl" class="absolute top-1/2 -translate-y-1/2 left-full w-10 aspect-[2/3] bg-kwdr-fg--muted flex justify-center items-center rounded-r-sm cursor-pointer" onclick={() => {sheetMode = !sheetMode}}>
     <RightArrow className={sheetMode ? 'rotate-180' : ''} />
