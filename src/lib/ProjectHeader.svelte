1bf427950f45eb2ea5d428623ab209be9adc640a
Samarth <samarth.tumdi@gmail.com>
2025-06-12 02:46:53 +0530

    ProjectHeader finalized

--- src/lib/ProjectHeader.svelte
+++ src/lib/ProjectHeader.svelte
@@ -8,32 +8,53 @@
     return `${semesterFull[0]}ester-${semesterFull[1]}`
   })
   
+  const isDesktop = () => {
+    return window.matchMedia('screen and (width >= 64rem)').matches;
+  }
 </script>
 
 
 {#snippet headerText(text: string, className?: string)}
-  <p class={['px-2 w-1/2', className]}>{text}</p>
+  <p class={['px-2', className]}>{text}</p>
 {/snippet}
 
-{#snippet headerAllScreens()}
-<div class="w-full flex flex-col lg:flex-row lg:justify-between">
-  <div class="flex lg:justify-between w-full lg:w-6/10">
-    {@render headerText(subjectName, 'text-left')}
-    {@render headerText(semester, 'text-right')}
-  </div>
-  {@render headerText(currentProject.projectName, 'w-full text-center lg:w-3/10 lg:text-right')}
-</div>
+{#snippet headerRevamped()}
+
+  {#if isDesktop()}
+    <div class="w-1/3">
+      {semester}
+    </div>
+    <div class="w-1/3 lg:text-center">
+      {currentProject.projectName}
+    </div>
+    <div class="w-1/3 lg:text-right">
+      {subjectName}
+    </div>
+
+  {:else}
+    <div class="text-center">
+      {semester} | {subjectName}
+    </div>
+    <div class="text-center">
+      {currentProject.projectName}
+    </div>
+  {/if}
+  
 {/snippet}
 
 <div
-  id="project-header"
-  class="relative w-3/4 left-1/2 -translate-x-1/2 px-0 lg:px-4 flex flex-col lg:flex-row gap-[5%] bg-kwdr-fg--muted text-2xs lg:text-xs text-kwdr-bg rounded-b-sm border-b-1 border-b-white/50"
+  class="w-full lg:px-4 flex justify-center text-2xs lg:text-xs text-kwdr-bg"
 >
-  {@render headerAllScreens()}
+  <div
+    id="header-content"
+    class="relative w-3/4 px-4 lg:flex lg:justify-between gap-[10%] bg-kwdr-fg--muted  rounded-b-sm border-b-1 border-b-white/50"
+  >
+  {@render headerRevamped()}
+  </div>
 </div>
 
 <style>
-  #project-header::before {
+  #header-content::before {
     --size: 10px;
     
     content: '';
@@ -45,7 +66,7 @@
     box-shadow: inset calc(-1 * var(--size)/2) calc(var(--size)/2) 0 0 var(--color-kwdr-fg--muted);
     clip-path: inset(50% 0% 0% 0%);
   }
-  #project-header::after {
+  #header-content::after {
     --size: 10px;
     
     content: '';
